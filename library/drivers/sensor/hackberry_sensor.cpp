5f55e5f0ab7db8838dcc8828b0fa6e1743602568
Thomas_Broussard <thomas.broussard91@gmail.com>
2019-04-23 19:03:15 +0200

    Adding the Sensor driver

---
+++ library/drivers/sensor/hackberry_sensor.cpp
@@ -0,0 +1,14 @@
+/* 
+ *  =============================================================================================================================================
+ *  Project : Hackberry e-Nable France
+ *  Author  : Thomas Broussard
+ * 
+ *  ---------------------------------------------------------------------------------------------------------------------------------------------
+ *  Description : Library for reading the Hackberry Hand Sensor
+ * 
+ *  Credits : 
+ *  Program inspired by the HACKberry project, created by exiii Inc.
+ *  https://github.com/mission-arm/HACKberry
+ * =============================================================================================================================================
+ */
+#include "hackberry_sensor.h"
