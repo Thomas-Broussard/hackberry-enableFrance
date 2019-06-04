e2bf039d769b5f4b0be2e261dc65ea9acaea698e
thomas-broussard <thomas.broussard91@gmail.com>
2019-06-04 20:24:52 +0200

    update Doxygen documentation

--- documentation/html/navtreedata.js
+++ documentation/html/navtreedata.js
@@ -23,24 +23,12 @@ for the JavaScript code in this file
 */
 var NAVTREE =
 [
-  [ "Hackberry - e-Nable France", "index.html", [
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
-    ] ]
-  ] ]
+  [ "Hackberry - e-Nable France", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_var_speed_servo_8h_source.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- documentation/html/search/searchdata.js
+++ documentation/html/search/searchdata.js
@@ -1,21 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "bcghimorsv",
-  1: "hsv",
-  2: "bcghimors"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions"
 };
 
