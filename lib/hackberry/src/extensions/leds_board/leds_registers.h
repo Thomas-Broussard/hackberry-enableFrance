80592ad37244ee19e381f81c410d8385ea909865
thomas-broussard <thomas.broussard91@gmail.com>
2019-06-27 20:12:25 +0200

    Mapping for led extension board modified

--- lib/hackberry/src/extensions/leds_board/leds_registers.h
+++ lib/hackberry/src/extensions/leds_board/leds_registers.h
@@ -5,31 +5,36 @@
 #define SET(reg,bit) reg |= (1<<bit)
 #define CLR(reg,bit) reg &=~(1<<bit)
 
-// turn on the GPIO
-#define X0_ON() SET(PORTD,3)   // D3
-#define X1_ON() SET(PORTC,5)   // A5
-#define X2_ON() SET(PORTC,4)   // A4
+// X0 (A5)
+#define X0_INIT() SET(DDRC,5)
+#define X0_ON() SET(PORTC,5)
+#define X0_OFF() CLR(PORTC,5)
 
-#define Y0_ON() CLR(PORTD,4)   // D4
-#define Y1_ON() CLR(PORTD,7)   // D7
-#define Y2_ON() CLR(PORTB,0)   // D8
+// X1 (A4)
+#define X1_INIT() SET(DDRC,4)
+#define X1_ON() SET(PORTC,4)
+#define X1_OFF() CLR(PORTC,4)
 
-// turn off the GPIO
-#define X0_OFF() CLR(PORTD,3)
-#define X1_OFF() CLR(PORTC,5)
-#define X2_OFF() CLR(PORTC,4)
+// X2 (A3)
+#define X2_INIT() SET(DDRC,3)
+#define X2_ON() SET(PORTC,3)
+#define X2_OFF() CLR(PORTC,3)
 
-#define Y0_OFF() SET(PORTD,4)
-#define Y1_OFF() SET(PORTD,7)
-#define Y2_OFF() SET(PORTB,0)
 
-// initialize GPIO as outputs
-#define X0_INIT() SET(DDRD,3)
-#define X1_INIT() SET(DDRC,5)
-#define X2_INIT() SET(DDRC,4)
+// Y0 (D3)
+#define Y0_INIT() SET(DDRD,3)
+#define Y0_ON() CLR(PORTD,3)
+#define Y0_OFF() SET(PORTD,3)
 
-#define Y0_INIT() SET(DDRD,4)
-#define Y1_INIT() SET(DDRD,7)
-#define Y2_INIT() SET(DDRB,0)
+// Y1 (D4)
+#define Y1_INIT() SET(DDRD,4)
+#define Y1_ON() CLR(PORTD,4)
+#define Y1_OFF() SET(PORTD,4)
+
+// Y2 (D0)
+#define Y2_INIT() SET(DDRD,0)
+#define Y2_ON() CLR(PORTD,0)   // D0
+#define Y2_OFF() SET(PORTD,0)
+
 
 #endif
