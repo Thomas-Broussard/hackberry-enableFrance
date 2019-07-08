d236d7c7f71ac732399b51a55ae9836aaf36498f
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-08 17:32:48 +0200

    Bluetooth instructions updated

--- lib/hackberry/src/extensions/bluetooth_board/bluetooth_instructions.h
+++ lib/hackberry/src/extensions/bluetooth_board/bluetooth_instructions.h
@@ -16,49 +16,46 @@
  *  https://github.com/mission-arm/HACKberry
  * =============================================================================================================================================
  */
+/*-----------------------------------
+* File generated with cmdlist.py
+-----------------------------------*/
 
 #ifndef __BLUETOOTH_INSTRUCTIONS_H__
 #define __BLUETOOTH_INSTRUCTIONS_H__
 
+//-----------------------------------
+//COMMAND LIST GEN
+//-----------------------------------
+	#define CMD_GEN_TEST 		0 	// check the bluetooth connection.
+	#define CMD_GEN_SET_PASS 	1 	// set new bluetooth password.
+	#define CMD_GEN_SET_NAME 	2 	// set new name.
+	#define CMD_GEN_STOP 		3 	// stop the bluetooth device.
+	#define CMD_GEN_RESET 		4 	// reset parameters.
+	#define CMD_GEN_VERSION 	5 	// get version of the current software.
+	#define CMD_GEN_BATTERY 	6 	// get the current level of battery (or -1 if battery can't be read).
 
-// General (AT commands)
-#define CMD_GEN_TEST        0 // check bluetooth connection
-#define CMD_GEN_SET_PASS    1 // set new password
-#define CMD_GEN_SET_NAME    2 // set new name
-#define CMD_GEN_STOP        3 // stop the bluetooth device
-#define CMD_GEN_RESET       4 // reset the parameters
+//-----------------------------------
+//COMMAND LIST SRV
+//-----------------------------------
+	#define CMD_SRV_MOVE_UP 	7 	// rotation of servomotors clockwise.
+	#define CMD_SRV_MOVE_DOWN 	8 	// rotation of servomotors reverse clockwise.
+	#define CMD_SRV_SAVE_MAX 	9 	// save current position as max limit.
+	#define CMD_SRV_SAVE_MIN 	10 	// save current position as min limit.
+	#define CMD_SRV_LOAD_MAX 	11 	// load max limit value.
+	#define CMD_SRV_LOAD_MIN 	12 	// load min limit value.
+	#define CMD_SRV_SET_HAND 	13 	// set type of hand.
+	#define CMD_SRV_GET_HAND 	14 	// get current type of hand.
+	#define CMD_SRV_CALIB 		15 	// launch servomotor calibration.
+	#define CMD_SRV_CALIB_UP 	16 	// move up the servomotor (calibration only).
+	#define CMD_SRV_CALIB_DOWN 	17 	// move down the servomotor (calibration only).
+	#define CMD_SRV_CALIB_OK 	18 	// validate the position of servomotor (calibration only).
 
-// Servomotors
-#define CMD_SRV_MOVE_UP     10 // rotation servo clockwise         CMD;member;degree
-#define CMD_SRV_MOVE_DOWN   11 // rotation servo reverse clockwise CMD;member;degree
+//-----------------------------------
+//COMMAND LIST SENS
+//-----------------------------------
+	#define CMD_SENS_GET_VALUE 	19 	// get current sensor value.
+	#define CMD_SENS_SET_TYPE 	20 	// set type of sensor used (IR,EMG,etc…).
+	#define CMD_SENS_GET_TYPE 	21 	// get type of sensor used (IR,EMG,etc…).
+	#define CMD_SENS_CALIB 		22 	// launch sensor calibration.
 
-#define CMD_SRV_SAVE_MAX    12 // save current position as max limit CMD;member;lim
-#define CMD_SRV_SAVE_MIN    13 // save current position as min limit CMD;member;lim
-
-#define CMD_SRV_LOAD_MAX    14 // load max limit value CMD
-#define CMD_SRV_LOAD_MIN    15 // load min limit value CMD
-
-#define CMD_SRV_SET_HAND    16 // set type of hand (right or left) CMD;hand
-#define CMD_SRV_GET_HAND    17 // get current type of hand CMD
-
-#define CMD_SRV_SET_SPEED   18 // set servomotor speed (0 - 100%) CMD;speed
-#define CMD_SRV_GET_SPEED   19 // get servomotor speed  CMD
-
-#define CMD_SRV_TEST        20 // test servomotors
-
-#define CMD_SRV_MOVE        21 // move servomotor to position CMD;member;position
-
-// Sensors
-#define CMD_SENS_GET_VALUE  30 // get current sensor value (MIN_ADC - MAX_ADC)
-
-#define CMD_SENS_SET_TYPE   31 // set type of sensor used (IR, EMG, etc..)
-#define CMD_SENS_GET_TYPE   32 // get type of sensor used (IR, EMG, etc..)
-
-#define CMD_SENS_CALIB      33 // launch calibration code
-
-
-
-
-
-/* end */
 #endif
