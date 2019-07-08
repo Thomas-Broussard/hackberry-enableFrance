bdedb03523a21b862fd286415ea6a76f91ffbf98
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-08 20:31:20 +0200

    bluetooth instructions updated

--- lib/hackberry/src/extensions/bluetooth_board/bluetooth_instructions.h
+++ lib/hackberry/src/extensions/bluetooth_board/bluetooth_instructions.h
@@ -16,6 +16,7 @@
  *  https://github.com/mission-arm/HACKberry
  * =============================================================================================================================================
  */
+
 /*-----------------------------------
 * File generated with cmdlist.py
 -----------------------------------*/
@@ -37,29 +38,29 @@
 //-----------------------------------
 //COMMAND LIST SRV
 //-----------------------------------
-	#define CMD_SRV_MOVE_UP 	7 	// rotation of servomotors clockwise.
-	#define CMD_SRV_MOVE_DOWN 	8 	// rotation of servomotors reverse clockwise.
-	#define CMD_SRV_SAVE_MAX 	9 	// save current position as max limit.
-	#define CMD_SRV_SAVE_MIN 	10 	// save current position as min limit.
-	#define CMD_SRV_LOAD_MAX 	11 	// load max limit value.
-	#define CMD_SRV_LOAD_MIN 	12 	// load min limit value.
-	#define CMD_SRV_SET_HAND 	13 	// set type of hand.
-	#define CMD_SRV_GET_HAND 	14 	// get current type of hand.
-	#define CMD_SRV_CALIB 		15 	// launch servomotor calibration.
-	#define CMD_SRV_CALIB_UP 	16 	// move up the servomotor (calibration only).
-	#define CMD_SRV_CALIB_DOWN 	17 	// move down the servomotor (calibration only).
-	#define CMD_SRV_CALIB_OK 	18 	// validate the position of servomotor (calibration only).
-    #define CMD_SRV_SET_SPEED   19
-    #define CMD_SRV_GET_SPEED   20
-    #define CMD_SRV_TEST        21
-    #define CMD_SRV_MOVE        22
+	#define CMD_SRV_MOVE 		7 	// move motor to the given position.
+	#define CMD_SRV_MOVE_UP 	8 	// rotation of servomotors clockwise.
+	#define CMD_SRV_MOVE_DOWN 	9 	// rotation of servomotors reverse clockwise.
+	#define CMD_SRV_SAVE_MAX 	10 	// save current position as max limit.
+	#define CMD_SRV_SAVE_MIN 	11 	// save current position as min limit.
+	#define CMD_SRV_LOAD_MAX 	12 	// load max limit value.
+	#define CMD_SRV_LOAD_MIN 	13 	// load min limit value.
+	#define CMD_SRV_SET_HAND 	14 	// set type of hand.
+	#define CMD_SRV_GET_HAND 	15 	// get current type of hand.
+	#define CMD_SRV_CALIB 		16 	// launch servomotor calibration.
+	#define CMD_SRV_CALIB_UP 	17 	// move up the servomotor (calibration only).
+	#define CMD_SRV_CALIB_DOWN 	18 	// move down the servomotor (calibration only).
+	#define CMD_SRV_CALIB_OK 	19 	// validate the position of servomotor (calibration only).
+	#define CMD_SRV_SET_SPEED 	20 	// Set the motors speed.
+	#define CMD_SRV_GET_SPEED 	21 	// Get the motors speed.
+	#define CMD_SRV_TEST 		22 	// Launch the test procedure for motors.
 
 //-----------------------------------
 //COMMAND LIST SENS
 //-----------------------------------
-	#define CMD_SENS_GET_VALUE 	19 	// get current sensor value.
-	#define CMD_SENS_SET_TYPE 	20 	// set type of sensor used (IR,EMG,etc…).
-	#define CMD_SENS_GET_TYPE 	21 	// get type of sensor used (IR,EMG,etc…).
-	#define CMD_SENS_CALIB 		22 	// launch sensor calibration.
+	#define CMD_SENS_GET_VALUE 	23 	// get current sensor value.
+	#define CMD_SENS_SET_TYPE 	24 	// set type of sensor used (IR,EMG,etc…).
+	#define CMD_SENS_GET_TYPE 	25 	// get type of sensor used (IR,EMG,etc…).
+	#define CMD_SENS_CALIB 		26 	// launch sensor calibration.
 
 #endif
